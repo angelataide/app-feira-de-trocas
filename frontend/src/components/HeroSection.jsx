5ae30fa45397a2fcd5739e6c2b7dbef8b9f3b506
Victor Felicio <victor_felicio@hotmail.com>
2025-06-30 21:15:21 -0300

    🐛Fix: conflito de branchs e removendo pacotes errados

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -1,6 +1,6 @@
 // Em src/components/HeroSection.jsx
-import React from 'react';
-import ArrowRight from "./ArrowRight.jsx";
+import React from 'react'
+import ArrowRight from './ArrowRight.jsx'
 import {
     HeroSectionWrapper,
     HeroTitle,
@@ -8,36 +8,37 @@ import {
     HeroIcon,
     HeroButtons,
     PrimaryButton,
-    SecondaryButton
-  } from './styles';
-import ExchangeIcon from './ExchangeIcon.jsx';
-import {Link} from 'react-router-dom';
-
+    SecondaryButton,
+} from './styles'
+import ExchangeIcon from './ExchangeIcon.jsx'
+import { Link } from 'react-router-dom'
 
 const HeroSection = () => {
-  return (
-    <HeroSectionWrapper>
-      <HeroIcon>
-        <ExchangeIcon />
-      </HeroIcon>
-      <HeroTitle>
-        Troque, Reutilize, <br />
-        <span>Conecte-se</span>
-      </HeroTitle>
-      <HeroSubtitle>
-        Transforme itens que você não usa mais em oportunidades de troca com seus <br />
-        vizinhos. Promova o consumo consciente e fortaleça os laços da sua comunidade.
-      </HeroSubtitle>
-      <HeroButtons>
-        <PrimaryButton as={Link} to="/itens">
-          Ver Itens Disponíveis <ArrowRight />
-        </PrimaryButton>
-        <SecondaryButton as={Link} to="/itens">
-          Começar a Trocar
-        </SecondaryButton>
-      </HeroButtons>
-    </HeroSectionWrapper>
-  );
-};
+    return (
+        <HeroSectionWrapper>
+            <HeroIcon>
+                <ExchangeIcon />
+            </HeroIcon>
+            <HeroTitle>
+                Troque, Reutilize, <br />
+                <span>Conecte-se</span>
+            </HeroTitle>
+            <HeroSubtitle>
+                Transforme itens que você não usa mais em oportunidades de troca
+                com seus <br />
+                vizinhos. Promova o consumo consciente e fortaleça os laços da
+                sua comunidade.
+            </HeroSubtitle>
+            <HeroButtons>
+                <PrimaryButton as={Link} to="/itens">
+                    Ver Itens Disponíveis <ArrowRight />
+                </PrimaryButton>
+                <SecondaryButton as={Link} to="/itens">
+                    Começar a Trocar
+                </SecondaryButton>
+            </HeroButtons>
+        </HeroSectionWrapper>
+    )
+}
 
-export default HeroSection;
+export default HeroSection
